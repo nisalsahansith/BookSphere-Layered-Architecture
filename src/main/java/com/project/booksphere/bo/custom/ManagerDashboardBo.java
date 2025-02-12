d8bd35587b74af7a403bb007c6b7edde068440a8
nisalsahansith <nisalsahansith@gmail.com>
2025-02-12 12:52:04 +0530

    Second-commit

--- src/main/java/com/project/booksphere/bo/custom/ManagerDashboardBo.java
+++
@@ -1,9 +0,0 @@
-package com.project.booksphere.bo.custom;
-
-import com.project.booksphere.bo.SuperBo;
-
-import java.sql.SQLException;
-
-public interface ManagerDashboardBo extends SuperBo {
-    public String getManagerName(String id) throws SQLException;
-}
